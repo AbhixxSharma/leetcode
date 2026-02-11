528cce41c5666e4e9af6f4bd3489bf2195a1d26c
Abhi <abhisheksharma92386@gmail.com>
2026-02-11 08:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (79.68%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum=0;
+    public TreeNode convertBST(TreeNode root) {
+        reverse(root);
+        return root;
+
+    }
+    public void reverse(TreeNode root){
+        if(root==null){
+            return;
+        }
+        reverse(root.right);
+        sum+=root.val;
+        root.val=sum;
+        reverse(root.left);
+
+    }
+}
+
+
+// public void reverse(TreeNode root){
+//         if(root==null) return ;
+
+//         reverse(root.right);
+//         sum+=root.val;
+//         root.val=sum;
+
+//         reverse(root.left);
+//     }
